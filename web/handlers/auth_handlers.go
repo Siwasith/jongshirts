2b70e8fb18bd5dfaab97d4f26ed424bbc2c2599a
sense475 <sensesense475@gmail.com>
2024-02-28 14:44:47 +0700

    feat add google authen

---
+++ web/handlers/auth_handlers.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/markbates/goth/gothic"
+	"github.com/oddsteam/jongshirts/internal/sessions"
+)
+
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	gothic.BeginAuthHandler(w, r)
+}
+
+func CallbackHandler(w http.ResponseWriter, r *http.Request) {
+	user, err := gothic.CompleteUserAuth(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Do something with the user, maybe register them/sign them in
+	_ = user
+	session, _ := sessions.NewSession(r)
+	// Authentication goes here
+	// ...
+	email := user.Email
+	// Set user as authenticated
+	session.Values["authenticated"] = true
+	session.Values["username"] = email
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
--- web/server.go
+++ web/server.go
@@ -3,8 +3,13 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"github.com/oddsteam/jongshirts/internal/sessions"
 	"github.com/oddsteam/jongshirts/web/handlers"
 )
 
@@ -12,8 +17,17 @@ import (
 
 func Start() {
 	fmt.Println("Starting server")
+	GOOGLE_CLIENT_ID := os.Getenv("GOOGLE_CLIENT_ID")
+	GOOGLE_CLIENT_SECRET := os.Getenv("GOOGLE_CLIENT_SECRET")
+	CALLBACK_URL := "http://localhost:8080/auth/google/callback"
+	gothic.Store = sessions.Store
+	goth.UseProviders(
+		google.New(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, CALLBACK_URL, "email"),
+	)
 	r := mux.NewRouter()
 
+	r.HandleFunc("/auth/{provider}", handlers.AuthHandler)
+	r.HandleFunc("/auth/{provider}/callback", handlers.CallbackHandler)
 	r.HandleFunc("/", handlers.HomeHandler)
 	r.HandleFunc("/cart", handlers.CartHandler).Methods("POST")
 	r.HandleFunc("/showcart", handlers.ShowCart)
