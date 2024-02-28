58238520b04942858e58e24ada861494aa5c94ba
ChampChonnakan <chonnakan@odds.team>
2024-02-28 11:42:49 +0700

    add dedicated user cart

--- web/handlers/cart_handlers.go
+++ web/handlers/cart_handlers.go
@@ -6,17 +6,20 @@ import (
 	"text/template"
 
 	"github.com/oddsteam/jongshirts/internal/db"
+	"github.com/oddsteam/jongshirts/internal/sessions"
 )
 
 func CartHandler(w http.ResponseWriter, r *http.Request) {
 	client := db.NewClient()
+	session, _ := sessions.NewSession(r)
+	username := session.Values["username"]
 	// var SelectedShirts []string
 
 	r.ParseForm()
 	// ctx := context.Background()
 
 	for key, _ := range r.Form {
-		client.LPush("selectedShirt", key)
+		client.LPush(username.(string), key)
 	}
 
 	http.Redirect(w, r, "/showcart", http.StatusSeeOther)
@@ -28,11 +31,15 @@ func ShowCart(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 
+	session, _ := sessions.NewSession(r)
+	username := session.Values["username"]
+
 	client := db.NewClient()
-	data, err := client.LRange("selectedShirt", 0, -1).Result()
+	data, err := client.LRange(username.(string), 0, -1).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
+	
 
 	fmt.Println(data)
 
